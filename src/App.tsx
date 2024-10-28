19a7bd7a092639d833827e75797d4056ab16f6f0
Luiz Vinicius <viniciusdgba2008@gmail.com>
2024-10-28 11:03:42 -0300

    feat: configurar roteamento com react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Login } from "./components/login";
 import { Register } from "./components/register";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export function App() {
 	return (
-		<>
-			<Register />
-		</>
+		<Router>
+			<Routes>
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</Router>
 	);
 }
