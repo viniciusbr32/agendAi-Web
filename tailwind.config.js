7a032c2f0e0a38fdba290c4a9b928f4c9bcf0373
Luiz Vinicius <viniciusdgba2008@gmail.com>
2024-10-29 21:22:52 -0300

    feat: instalar o shadcn e adicionar os componentes de table e dropMenu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: ['class'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        blueCustom: '#0D6EFD',
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
+  	extend: {
+  		colors: {
+  			blueCustom: '#0D6EFD'
+  		},
+  		fontFamily: {
+  			poppins: ['Poppins', 'sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
