eaaee9bc0beff1a83a45f5ba59db72820e2fa044
Luiz Vinicius <viniciusdgba2008@gmail.com>
2024-10-29 21:32:32 -0300

    feat: criar constantes e definir tipos para a constante

---
+++ src/types/dataType.ts
@@ -0,0 +1,23 @@
+export type Doctor = {
+	id_doctor: number;
+	name: string;
+	specialty: string;
+	icon: "M" | "F";
+};
+
+export type appointment = {
+	id_appointment: number;
+	service: string;
+	doctor: string;
+	specialty: string;
+	booking_date: string;
+	booking_hour: string;
+	user: string;
+	price: number;
+};
+
+export type DoctorService = {
+	id_service: number;
+	description: string;
+	price: number;
+};
